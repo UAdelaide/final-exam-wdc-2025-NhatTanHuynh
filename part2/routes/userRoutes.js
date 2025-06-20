386d063652aa13fd155d692953d7137b20c17701
NhatTanHuynh <a1948706@adelaide.edu.au>
2025-06-20 13:11:47 +0000

    [automatic commit] 2025-06-20 13:11:47 UTC 58.174.112.100

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,7 +49,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    req.session.user = rows[0]; // dele
+    req.session.user = rows[0]; // delete
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
