ae152a5bc4365c031de3de001d5575075d4f372d
NhatTanHuynh <a1948706@adelaide.edu.au>
2025-06-20 10:22:05 +0000

    [automatic commit] 2025-06-20 10:22:04 UTC 58.174.112.100

--- part1/app.js
+++ part1/app.js
@@ -45,7 +45,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
 //Question7
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
-        const query = 'SELECT wr.request_id, d.name AS dog_name, wr.requested_time, ';
+        const query = 'SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, we.location';
     } catch (error) {
 
     }
