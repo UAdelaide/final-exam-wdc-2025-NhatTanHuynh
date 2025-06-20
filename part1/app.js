e632398a90b5183bcce1434f20fb764c29a3586f
NhatTanHuynh <a1948706@adelaide.edu.au>
2025-06-20 10:26:52 +0000

    [automatic commit] 2025-06-20 10:26:52 UTC 58.174.112.100

--- part1/app.js
+++ part1/app.js
@@ -47,6 +47,6 @@ app.get('/api/walkrequests/open', async (req, res) => {
     try {
         const query = 'SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username FROM WalkRequests wr JOIN Dogs d ON wr.dog_id = d.dog_id JOIN Users u ON d.owner_id = u.user_id WHERE wr.status = "open" ORDER BY wr.requested_time';
     } catch (error) {
-
+        console.error()
     }
 });
