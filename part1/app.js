2bb0fa45370634f2822a940e59f8106ef8a9fc6d
NhatTanHuynh <a1948706@adelaide.edu.au>
2025-06-20 10:27:53 +0000

    [automatic commit] 2025-06-20 10:27:53 UTC 58.174.112.100

--- part1/app.js
+++ part1/app.js
@@ -47,8 +47,8 @@ app.get('/api/walkrequests/open', async (req, res) => {
     try {
         const query = 'SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username FROM WalkRequests wr JOIN Dogs d ON wr.dog_id = d.dog_id JOIN Users u ON d.owner_id = u.user_id WHERE wr.status = "open" ORDER BY wr.requested_time';
         const [results] = await promiseDb.query(query);
-        res.json(result)
+        res.json(results);
     } catch (error) {
-        console.error()
+        console.error('')
     }
 });
