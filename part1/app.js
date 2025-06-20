b111c66c4517a8e4f16f4348c26527a2be2d4225
NhatTanHuynh <a1948706@adelaide.edu.au>
2025-06-20 10:35:48 +0000

    [automatic commit] 2025-06-20 10:35:48 UTC 58.174.112.100

--- part1/app.js
+++ part1/app.js
@@ -57,7 +57,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
 //Question8
 app.get('/api/walkers/summary', async (req, res) => {
     try {
-        const query = 'SELECT u.user_id walker_username, COUNT (r.reating) AS total_ratings, ';
+        const query = 'SELECT u.user_id walker_username, COUNT (r.reating) AS total_ratings, AVG(r.rating) AS average_rating,';
     } catch (error) {
         console.error("database error", error);
         res.status(500).json({ error: 'Failed to fetch walker summary' });
