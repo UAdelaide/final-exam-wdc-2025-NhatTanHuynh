f224240f72358f6c00b1fd29d97f423865659d46
NhatTanHuynh <a1948706@adelaide.edu.au>
2025-06-20 10:14:36 +0000

    [automatic commit] 2025-06-20 10:14:36 UTC 58.174.112.100

--- part1/app.js
+++ part1/app.js
@@ -32,6 +32,7 @@ module.exports = app;
 
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
-        const query = 'SELECT d.name as dag_name. d.size, u.username as owner_usernsme FROM Dogs d JOIN Users
+        const query = 'SELECT d.name as dag_name. d.size, u.username as owner_usernsme FROM Dogs d
+        J'
     }
 });
